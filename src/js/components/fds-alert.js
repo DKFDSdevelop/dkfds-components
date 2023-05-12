571f1a75eb9ce1c261aa2d6834ed90dfe2d3274b
Helle Juhl <hef@netcompany.com>
2023-05-12 14:29:01 +0200

    Added listener to close button

--- src/js/components/fds-alert.js
+++ src/js/components/fds-alert.js
@@ -21,6 +21,8 @@ function isHeadingtypeValid(headingtype) {
 }
 
 class FDSAlert extends HTMLElement {
+    #closeClickhandler;
+
     static get observedAttributes() {
         return ['variant', 'heading', 'headingtype', 'closeable'];
     }
@@ -86,6 +88,7 @@ class FDSAlert extends HTMLElement {
 
     constructor() {
         super();
+        this.#closeClickhandler = () => { this.hide() };
     }
 
     hide() {
@@ -103,7 +106,7 @@ class FDSAlert extends HTMLElement {
         /* Create the base for the alert */
         let constructedAlert = document.createElement('div');
         constructedAlert.classList.add('alert');
-        
+
         /* Add variant */
         if (isVariantValid(this.variant)) {
             constructedAlert.classList.add("alert-" + this.variant);
@@ -115,7 +118,7 @@ class FDSAlert extends HTMLElement {
             alertHeading.textContent = this.heading;
             alertHeading.classList.add('alert-heading');
             constructedAlert.appendChild(alertHeading);
-        }        
+        }
         else {
             let alertHeading = document.createElement('div');
             alertHeading.textContent = this.heading;
@@ -133,9 +136,31 @@ class FDSAlert extends HTMLElement {
             alertBody.innerHTML = this.innerHTML;
         }
         constructedAlert.appendChild(alertBody);
-        
+
+        /* Add close button */
+        if (this.closeable) {
+            let closeButton = document.createElement('button');
+            closeButton.type = 'button';
+            closeButton.classList.add('alert-close');
+            closeButton.innerHTML = '<svg class="icon-svg" focusable="false" aria-hidden="true" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"></path></svg>Luk';
+            constructedAlert.appendChild(closeButton);
+            constructedAlert.querySelector('.alert-heading').classList.add('pr-8');
+        }
+
         /* Finish the alert */
         this.innerHTML = constructedAlert.outerHTML;
+
+        if (this.closeable) {
+            /* In case there's an alert inside an alert, ensure that we find the correct close button */
+            if (this.children.length > 0) {
+                let alertElements = this.children[0].children;
+                for (let i = 0; i < alertElements.length; i++) {
+                    if (alertElements[i].classList.contains('alert-close')) {
+                        alertElements[i].addEventListener("click", this.#closeClickhandler);
+                    }
+                }
+            }
+        }
 
     }
 
@@ -145,7 +170,6 @@ class FDSAlert extends HTMLElement {
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === "variant") {
             if (this.querySelector('.alert')) {
-
                 let classes = this.querySelector('.alert').classList;
                 classes.remove('alert-info');
                 classes.remove('alert-success');
@@ -155,10 +179,6 @@ class FDSAlert extends HTMLElement {
                 if (isVariantValid(newValue)) {
                     this.querySelector('.alert').classList.add("alert-" + newValue);
                 }
-                else {
-                    throw new Error("Invalid variant at attribute change: '" + newValue + "'. Variant must be 'info', 'success', 'warning' or 'error'.");
-                }
-
             }
         }
         if (name === "heading") {
@@ -175,8 +195,29 @@ class FDSAlert extends HTMLElement {
                     heading.classList.add('alert-heading');
                     this.querySelector('.alert').replaceChild(heading, this.querySelector('.alert-heading'));
                 }
-                else if (!isHeadingtypeValid(newValue)) {
-                    throw new Error("Invalid heading type at attribute change: '" + val + "'. Valid values are 'h1', 'h2, 'h3', 'h4', 'h5', 'h6' and 'strong'.");
+            }
+        }
+        if (name === "closeable") {
+            if (!this.closeable) {
+                /* In case there's an alert inside an alert, ensure that we find the correct close button */
+                if (this.children.length > 0) {
+                    let alertElements = this.children[0].children;
+                    for (let i = 0; i < alertElements.length; i++) {
+                        if (alertElements[i].classList.contains('alert-close')) {
+                            alertElements[i].removeEventListener("click", this.#closeClickhandler);
+                        }
+                    }
+                }
+            }
+            else {
+                /* In case there's an alert inside an alert, ensure that we find the correct close button */
+                if (this.children.length > 0) {
+                    let alertElements = this.children[0].children;
+                    for (let i = 0; i < alertElements.length; i++) {
+                        if (alertElements[i].classList.contains('alert-close')) {
+                            alertElements[i].addEventListener("click", this.#closeClickhandler);
+                        }
+                    }
                 }
             }
         }
