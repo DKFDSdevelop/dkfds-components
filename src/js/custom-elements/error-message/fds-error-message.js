f4cc8b540c9abea3d3892360015168e6d9ab4f2c
Helle Juhl <hef@netcompany.com>
2025-11-26 14:06:41 +0100

    Simplified CE error-message

--- src/js/custom-elements/error-message/fds-error-message.js
+++ src/js/custom-elements/error-message/fds-error-message.js
@@ -7,23 +7,15 @@ class FDSErrorMessage extends HTMLElement {
     /* Private instance fields */
 
     #rendered;
-    #errorText;
     #srOnlyText;
     #parentWrapper;
 
-    #getErrorText() {
-        if (this.#errorText) return this.#errorText;
+    #render() {
+        if (this.#rendered) return;
 
-        this.#errorText = this.querySelector(':scope > .form-error-message');
-        return this.#errorText;
-    }
-
-    #ensureSrOnlyPrefix() {
-        const span = this.#getErrorText();
-        if (!span) return;
-
-        const firstElement = span.firstElementChild;
-        if (!firstElement || !firstElement.classList.contains('sr-only')) {
+        const hasElements = this.children.length > 0;
+        
+        if (!hasElements) {
             const srText = this.getAttribute('sr-text');
             if (srText !== null && srText !== '') {
                 this.#srOnlyText = srText;
@@ -33,52 +25,21 @@ class FDSErrorMessage extends HTMLElement {
             sr.className = 'sr-only';
             sr.textContent = `${this.#srOnlyText}: `;
 
-            span.insertBefore(sr, span.firstChild);
+            const visibleMessage = document.createElement('span');
+            visibleMessage.className = 'visible-message';
+            visibleMessage.textContent = this.textContent;
+            this.textContent = '';
+
+            this.appendChild(sr);
+            this.appendChild(visibleMessage);
         }
-    }
-
-    #render() {
-        if (this.#rendered) return;
-
-        let span = this.#getErrorText();
-        if (!span) {
-            span = document.createElement('span');
-            span.className = 'form-error-message';
-
-            // Move existing child nodes into the span
-            while (this.firstChild) {
-                span.appendChild(this.firstChild);
-            }
-
-            this.appendChild(span);
-            this.#errorText = span;
-        }
-
-        // If explicit id attribute is set, use it
-        const attrValue = this.getAttribute('error-message-id');
-        if (attrValue !== null && attrValue !== '') {
-            span.id = attrValue;
-        }
-
-        this.#ensureSrOnlyPrefix();
 
         this.#rendered = true;
     }
 
-    #updateId(newValue) {
-        const span = this.#getErrorText();
-        if (!span) return;
-
-        if (newValue !== null && newValue !== '') {
-            span.id = newValue;
-        } else {
-            span.id = generateAndVerifyUniqueId('error');
-        }
-    }
-
     /* Attributes which can invoke attributeChangedCallback() */
 
-    static observedAttributes = ['error-message-id', 'sr-text'];
+    static observedAttributes = ['id', 'sr-text'];
 
     /* --------------------------------------------------
     CUSTOM ELEMENT CONSTRUCTOR (do not access or add attributes in the constructor)
@@ -87,7 +48,6 @@ class FDSErrorMessage extends HTMLElement {
     constructor() {
         super();
         this.#rendered = false;
-        this.#errorText = null;
         this.#srOnlyText = 'Fejl';
         this.#parentWrapper = null;
     }
@@ -101,9 +61,8 @@ class FDSErrorMessage extends HTMLElement {
 
         this.#render();
 
-        const span = this.#getErrorText();
-        if (span && !span.id) {
-            span.id = generateAndVerifyUniqueId('error');
+        if (!this.id) {
+            this.id = generateAndVerifyUniqueId('error');
         }
 
         // Save reference to parent wrapper
@@ -119,7 +78,6 @@ class FDSErrorMessage extends HTMLElement {
     disconnectedCallback() {
         this.#parentWrapper?.dispatchEvent(new Event('error-message-callback'));
 
-        this.#errorText = null;
         this.#parentWrapper = null;
         this.#rendered = false;
     }
@@ -130,10 +88,6 @@ class FDSErrorMessage extends HTMLElement {
 
     attributeChangedCallback(name, oldValue, newValue) {
         if (!this.#rendered) return;
-
-        if (name === 'error-message-id') {
-            this.#updateId(newValue);
-        }
 
         if (name === 'sr-text') {
             this.#srOnlyText = newValue;
