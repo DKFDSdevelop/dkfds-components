b8c6cb4bcbdf4afe45bd89ce2b1a9e59245fe076
Helle Juhl <hef@netcompany.com>
2026-08-22 00:42:16 +0200

    Added scrollable and fixed areas to fds-modal

--- dist/custom-elements/dkfds.js
+++ dist/custom-elements/dkfds.js
@@ -5923,6 +5923,7 @@ class FDSDrawer extends HTMLElement {
     if (!this.hasAttribute('open') || this.getAttribute('open') === 'false') {
       this.setAttribute('open', '');
       document.addEventListener('fds.modal.shown', this.#handleCloseClick, false);
+      document.addEventListener('fds-modal-opener-click', this.#handleCloseClick, false);
       document.addEventListener('keydown', this.#handleKeydown, false);
       this.querySelector('.button-menu-close')?.focus();
     }
@@ -5932,6 +5933,7 @@ class FDSDrawer extends HTMLElement {
     if (this.hasAttribute('open')) {
       this.removeAttribute('open');
       document.removeEventListener('fds.modal.shown', this.#handleCloseClick, false);
+      document.removeEventListener('fds-modal-opener-click', this.#handleCloseClick, false);
       document.removeEventListener('keydown', this.#handleKeydown, false);
       const drawerOpener = document.querySelector(`fds-drawer-opener[drawer=${this.id}] button`);
       const visibleDrawerOpener = isVisibleAndFocusable(drawerOpener);
@@ -5960,6 +5962,7 @@ class FDSDrawer extends HTMLElement {
     this.querySelector('.overlay')?.removeEventListener('click', this.#handleCloseClick, false);
     this.querySelector('.mobile-drawer').removeEventListener('click', this.#handleDrawerLinkClick, false);
     document.removeEventListener('fds.modal.shown', this.#handleCloseClick, false);
+    document.removeEventListener('fds-modal-opener-click', this.#handleCloseClick, false);
     document.removeEventListener('keydown', this.#handleKeydown, false);
     if (this.#resizeObserver) {
       this.#resizeObserver.disconnect();
